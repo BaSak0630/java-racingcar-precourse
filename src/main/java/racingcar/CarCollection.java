d9c7d06b2dc6fadf7f3baf284db359c4587dd84f
BaSak0630 <babamba125@gmail.com>
2024-07-11 01:04:11 +0900

    CarCollection : 자동차들을 관리 하는 객체 생성

---
+++ src/main/java/racingcar/CarCollection.java
@@ -0,0 +1,32 @@
+package racingcar;
+
+import java.util.List;
+
+/*
+ * 클래스 이름 CarCollection
+ *
+ * 버전 정보 V1
+ *
+ * 날짜 7월 10일
+ *
+ * 저작권 주의
+ */
+public class CarCollection {
+    private static final int CAR_NAME_LENGTH = 5;
+    private List<Car> cars;
+
+    public CarCollection() {
+
+    }
+    public CarCollection(List<Car> cars) {
+        validationNameSize(cars);
+    }
+
+    private void validationNameSize(List<Car> cars) {
+        for(Car car : cars) {
+            if(car.validationName(CAR_NAME_LENGTH)){
+                throw new IllegalArgumentException("[ERROR] 자동차의 이름은 5자 이하만 가능하다.");
+            }
+        }
+    }
+}
